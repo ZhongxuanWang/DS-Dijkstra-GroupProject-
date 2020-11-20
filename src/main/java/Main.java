d424444915ac747dbc0b5b753ef04365feb18250
wangzhongxuan <cndanielwang@gmail.com>
2020-11-21 04:16:56 +0800

    some update!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 
 import org.jgrapht.*;
+import org.jgrapht.generate.*;
 import org.jgrapht.graph.*;
 // import org.jgrapht.nio.*;
 // import org.jgrapht.nio.dot.*;
@@ -10,23 +11,16 @@ import java.net.*;
 import java.rmi.server.ExportException;
 import java.util.*;
 
-
-
-
-/**
- * Just want to play with Gradle :)
- */
-
 public class Main {
     public static boolean stop = false;
     public static boolean debug = false;
 
     public static void main(String[] args) throws IOException, ExportException, URISyntaxException {
 
-        Graph<String, DefaultEdge> stringGraph = createStringGraph();
+        Graph<String, DefaultEdge> stringGraph;
 
         System.out.println("-- toString output");
-        System.out.println(stringGraph.toString());
+        // System.out.println(stringGraph.toString());
         System.out.println();
 
 
@@ -55,36 +49,4 @@ public class Main {
             return "";
         }
     }
-
-    private static Graph<String, DefaultEdge> createStringGraph()
-    {
-        Graph<String, DefaultEdge> g = new SimpleGraph<>(DefaultEdge.class);
-
-        String v = "THANKSGIVING";
-
-        StringTokenizer st = new StringTokenizer(v);
-
-        while (st.hasMoreTokens()) {
-            g.addVertex(st.nextToken());
-        }
-        st = new StringTokenizer(v);
-        while (st.hasMoreTokens()) {
-            g.addEdge(st.nextToken());
-        }
-
-        // add the vertices
-        g.addVertex(v1);
-        g.addVertex(v2);
-        g.addVertex(v3);
-        g.addVertex(v4);
-
-        // add edges to create a circuit
-        g.addEdge(v1, v2);
-        g.addEdge(v2, v3);
-        g.addEdge(v3, v4);
-        g.addEdge(v4, v1);
-
-        return g;
-    }
-
 }
--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -1,59 +1,28 @@
+import org.jgrapht.Graph;
+import org.jgrapht.generate.GnmRandomGraphGenerator;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+
 import java.util.HashMap;
 
 public class Map {
 
-    /**
-     * Map information:
-     * - Total Node: 14
-     * - Max Connection of each node: 5
-     */
-
-    // Starting point
-    Node root = new Node();
-    int distanceSum = 0;
+    Graph<String, DefaultEdge> graph = new DefaultDirectedGraph<>(DefaultEdge.class);
 
 
-    public Map(boolean random_distance) {
-        int[] distances = {
-                3, 1, 12, 14, 10, 5, 3, 9, 7, 2, 6, 11, 13, 2, 4,
-                // must be more...
-        };
-        if (random_distance) {
-            for (int i = 0; i < 14; i ++) {
-                distances[i] = (int) (Math.random() * 15 + 1);
-            }
+    Map (boolean random) {
+        GnmRandomGraphGenerator<String, DefaultEdge> g;
+        if (random) {
+            g = new GnmRandomGraphGenerator<>(12,24);
+        } else {
+            g = new GnmRandomGraphGenerator<>(12,24,1);
         }
-        Node[] nodes = new Node[14];
-
-        for (int i = 0; i < 14; i ++) {
-            nodes[i] = new Node();
-        }
-    }
-
-    public void summary() {
-        System.out.println("Distance Covered so far:\t" + distanceSum);
+        g.generateGraph(graph);
     }
 
 
-
-    static class Node {
-        HashMap<Node, Integer> linkedNodes = new HashMap<>(5,1); // Max 5 connections
-        String name;
-
-        Node () {
-
-        }
-
-        @Override
-        public String toString() {
-            return "Node";
-        }
-
-        // @Override
-        // public int compareTo(Node o) {
-        //     int distance = linkedNodes.get(o);
-        //     if ()
-        //     return 0;
-        // }
+    private static Graph<String, DefaultEdge> createStringGraph()
+    {
+        return null;
     }
 }
